84f34c184776eb785c311b22741fcca597a04ab5
Marcello Magno <marcello_magno@hotmail.com>
2024-10-30 16:48:26 -0300

    config connection sqlite

---
+++ src/models/sqlite/settings/connection_test.py
@@ -0,0 +1,14 @@
+import pytest
+from .connection import db_connection_handler
+from sqlalchemy import Engine
+
+@pytest.mark.skip(reason="teste de integração com banco")
+def test_connect_to_db():
+    assert db_connection_handler.get_engine() is None
+
+    db_connection_handler.connect_to_db()
+    db_engine = db_connection_handler.get_engine()
+
+    assert db_engine is not None
+    assert isinstance(db_engine, Engine)
+    
