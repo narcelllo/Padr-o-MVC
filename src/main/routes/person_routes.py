0501fbe7dad5d136d7dac5e244bffb456a9e4a41
Marcello Magno <marcello_magno@hotmail.com>
2024-12-02 09:09:50 -0300

    Implementing routes pets and people

--- src/main/routes/person_routes.py
+++ src/main/routes/person_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from src.views.http_types.http_request import HttpRequest
 from src.main.composer.person_creator_composer import person_creator_composer
+from src.main.composer.person_finder_composer import person_finder_composer
 
 person_route_bp = Blueprint("person_routes", __name__)
 
@@ -10,4 +11,12 @@ def create_person():
     view = person_creator_composer()
     http_response = view.handle(http_request)
 
+    return jsonify(http_response.body), http_response.status_code
+
+@person_route_bp.route("/people/<person_id>", methods=["GET"])
+def finder_person(person_id):
+    http_request = HttpRequest(param={"person_id": person_id})
+    view = person_finder_composer()
+    
+    http_response = view.handle(http_request)
     return jsonify(http_response.body), http_response.status_code
